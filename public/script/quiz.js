772401636653e22a4244c20e8ec8211fe6f8f788
Dasari Nandini <dasarinandini@Dasaris-MacBook-Pro.local>
2026-03-07 11:26:48 +0530

    creates a mockdata of fetch and writes fetch and display functions.

--- public/script/quiz.js
+++ public/script/quiz.js
@@ -2,22 +2,23 @@ import { createNestFragment, Elements } from "./dom.js";
 
 const { ARTICLE, FORM, FIELDSET, LEGEND, H2, INPUT, DIV, LABEL } = Elements;
 
-const options = ['Ooty', 'pakistan', 'delhi']
-const divs = options.map((option, index) =>
-  [
-    DIV, {},
+const divNodeStructure = (options) => {
+  return options.map((option, index) =>
     [
+      DIV, {},
       [
-        INPUT, { type: 'radio', id: `option-${index + 1}`, name: 'options' },
-        ''
-      ],
-      [
-        LABEL, { for: `option-${index + 1}` }, `${option}`,
+        [
+          INPUT, { type: 'radio', id: `option-${index + 1}`, name: 'options' },
+          ''
+        ],
+        [
+          LABEL, { for: `option-${index + 1}` }, `${option}`,
+        ]
       ]
-    ]
-  ])
+    ])
+}
 
-const articleNode =
+const articleNodeStructure = (divs) =>
   [
     ARTICLE, {},
     [
@@ -45,8 +46,23 @@ const articleNode =
     ]
   ]
 
-window.onload = () => {
+const displayQuestions = (questions) => {
   const section = document.querySelector('section')
+  const divs = divNodeStructure(questions.options)
+  const articleNode = articleNodeStructure(divs)
   section.append(createNestFragment(...articleNode));
   console.log(section);
 }
+
+const fetchQuestions = async () => {
+  return {
+    question: '1.What is the captial of India?',
+    options: ['india', 'pakistan', 'new delhi', 'andhara pradesh']
+  }
+}
+
+const main = () => {
+  fetchQuestions().then(displayQuestions);
+}
+
+window.onload = main;
