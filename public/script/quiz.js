7f706dfb5ad21480fc610f0c602821a00c2b505d
Dasari Nandini <dasarinandini@Dasaris-MacBook-Pro.local>
2026-03-07 16:10:47 +0530

    adds Quiz class for maintaining quiz state.
    * in quiz State we have get question, next qyuestion , validating user answer and increase score.

--- public/script/quiz.js
+++ public/script/quiz.js
@@ -1,4 +1,5 @@
 import { createNestFragment, Elements } from "./dom.js";
+import { Quiz } from "./quiz_state.js";
 
 const { ARTICLE, FORM, FIELDSET, LEGEND, H2, INPUT, DIV, LABEL } = Elements;
 
@@ -9,7 +10,7 @@ const divNodeStructure = (options) => {
   return options.map((option, i) => [
     DIV, {},
     [
-      [INPUT, { type: 'radio', id: `option-${i + 1}`, name: 'options' }, ''],
+      [INPUT, { type: 'radio', id: `option-${i + 1}`, name: 'options', value: option }, ''],
       [LABEL, { for: `option-${i + 1}` }, option]
     ]
   ]);
@@ -36,55 +37,64 @@ const articleNodeStructure = (divs, question) => [
     ]
   ]
 ]
+const listener = (section, quizState) => {
+  const form = section.querySelector('form');
+  form.addEventListener('submit',
+    (e) => {
+      const formData = new FormData(e.target);
+      const selectedValue = formData.get("options");
+      e.preventDefault();
+      console.log(quizState)
+      const section = document.querySelector('section');
 
-const displayQuestion = (question) => {
+      while (section.firstChild) {
+        section.removeChild(section.firstChild);
+      }
+      quizState.validateAnswer(selectedValue);
+      quizState.nextQuestion();
+      if (quizState.isQuizFinish()) {
+        displayQuestion(quizState.getQuestion(), quizState);
+      }
+      else {
+        console.log(quizState.getScore());
+      }
+    }
+  );
+}
+const displayQuestion = (question, quizState) => {
   const section = document.querySelector('section');
 
   const divs = divNodeStructure(question.options);
   const articleNode = articleNodeStructure(divs, question);
 
   section.append(createNestFragment(...articleNode));
-
-  const form = section.querySelector('form');
-  form.addEventListener('submit', handleSubmit);
+  listener(section, quizState)
 }
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-
-  const section = document.querySelector('section');
-
-  while (section.firstChild) {
-    section.removeChild(section.firstChild);
-  }
-
-  index++;
-
-  if (index < questions.length) {
-    displayQuestion(questions[index]);
-  }
-}
-
 const fetchQuestions = async () => {
   return [
     {
       question: '1.What is the capital of India?',
-      options: ['india', 'pakistan', 'new delhi', 'andhra pradesh']
+      options: ['india', 'pakistan', 'new delhi', 'andhra pradesh'],
+      answer: 'india',
     },
     {
       question: '2.What is the capital of Pakistan?',
-      options: ['india', 'pakistan', 'andhra pradesh', 'agra']
+      options: ['india', 'pakistan', 'andhra pradesh', 'agra'],
+      answer: 'pakistan'
+
     },
     {
       question: '3.What is the capital of Andhra Pradesh?',
-      options: ['india', 'pakistan', 'andhra pradesh', 'amaravathi']
+      options: ['india', 'pakistan', 'andhra pradesh', 'amaravathi'],
+      answer: 'amaravathi'
     }
   ];
 }
 
 const main = async () => {
   questions = await fetchQuestions();
-  displayQuestion(questions[index]);
+  const quizState = new Quiz(questions);
+  displayQuestion(quizState.getQuestion(), quizState);
 }
 
 window.onload = main;
---
+++ public/script/quiz_state.js
@@ -0,0 +1,32 @@
+export class Quiz {
+  #quizState;
+  constructor(questions) {
+    this.#quizState = {};
+    this.#quizState.questions = questions;
+    this.#quizState.index = 0;
+    this.#quizState.score = 0;
+  }
+
+  getQuestion() {
+    return this.#quizState.questions[this.#quizState.index]
+  }
+
+  nextQuestion() {
+    this.#quizState.index++;
+  }
+
+  validateAnswer(answer) {
+    if (this.#quizState.questions[this.#quizState.index].answer === answer) {
+      this.#quizState.score++;
+    }
+  }
+
+  isQuizFinish() {
+    return this.#quizState.index < this.#quizState.questions.length
+  }
+
+  getScore() {
+    return this.#quizState.score;
+  }
+
+}
